01bbefc74fb0b85628b4fe367d78658c147cfd8f
Sobreira <85771661+Gustavo-Sobreira@users.noreply.github.com>
2021-07-06 21:08:49 -0300

    Ideias

---
+++ JogoDaVelha01.py
@@ -0,0 +1,27 @@
+#Sobreira Gustavo
+
+#Passos:
+#1- Entarda
+#2- Escolher emoji
+#3- Tirar na sorte o primeiro jogador
+#4- Adcionar jogada na matriz
+#3.1- Jogador 2 joga
+#4.1- Adicionar jogada na matriz
+#5- Verifica se ultimo a jogar ganhou
+#6- Caso jogadas for igual a 9 deu velha
+#Extas - Música, som de cada jogada, comemoração do vencedor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
